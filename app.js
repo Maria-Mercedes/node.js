08b3bb3145625b386220c79aa8551534abb4eab6
Maria Mercedes <rodriguesmaria860@gmail.com>
2024-09-08 20:24:36 -0300

    endpoints

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log("oiiiiii!")
+import express from 'express'
+
+const servidor = express();
+
+//criando um endpoint
+//var, método, caminho, request and response, arrowfunction
+servidor.get('/helloworld', (req, resp) => {
+    //código endpoint: oq será executado
+
+    resp.send('Hello World!')
+})
+
+servidor.get('/mensagem/boasvindas', (req, resp) => {
+    resp.send('Olá, seja bem-vinde!')
+})
+
+//vincular a API a uma porta (nesse caso, 5001)
+//Se o vínculo der certo, a mensagem é exibida.
+servidor.listen(5001, () => console.log("API subiu!!")) 
 
